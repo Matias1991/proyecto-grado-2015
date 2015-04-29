faa31933524c8e56f0b4c3c9a8671a3d4af46d04
Matias1234 <matiasacosta91@gmail.com>
2015-04-29 17:00:55 -0300

    Add new comment

--- branches/WebService/src/com/example/service/User.java
+++ branches/WebService/src/com/example/service/User.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class User implements Serializable{
 
 	/**
-	 * 
+	 * prueba comentario
 	 */
 	private static final long serialVersionUID = 1L;
 	private int id;
