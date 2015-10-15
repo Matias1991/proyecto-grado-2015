f7c4c9abdc52b4b6851971b7e4199244da49c918
yamilaghan <Proyectorado2015>
2015-10-14 22:07:43 -0300

    Servicios para mobile y condición defensiva

--- WebService/src/servicelayer/service/ServiceMobile.java
+++ WebService/src/servicelayer/service/ServiceMobile.java
@@ -1,22 +1,28 @@
 package servicelayer.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import servicelayer.core.CoreBill;
 import servicelayer.core.CoreCharge;
 import servicelayer.core.CoreProject;
+import servicelayer.core.CoreProjectLiquidation;
 import servicelayer.core.CoreUser;
 import servicelayer.entity.businessEntity.ChanelType;
 import servicelayer.entity.businessEntity.Charge;
+import servicelayer.entity.businessEntity.ProjectLiquidation;
 import servicelayer.entity.businessEntity.User;
 import servicelayer.entity.valueObject.VOBill;
+import servicelayer.entity.valueObject.VOCategory;
 import servicelayer.entity.valueObject.VOCharge;
 import servicelayer.entity.valueObject.VOProject;
+import servicelayer.entity.valueObject.VOProjectLiquidation;
 import servicelayer.entity.valueObject.VOUser;
 import servicelayer.service.builder.BillBuilder;
 import servicelayer.service.builder.ChargeBuilder;
 import servicelayer.service.builder.ProjectBuilder;
+import servicelayer.service.builder.ProjectLiquidationBuilder;
 import servicelayer.service.builder.UserBuilder;
 import servicelayer.utilities.Constants;
 import shared.exceptions.ClientException;
@@ -24,6 +30,7 @@ import shared.exceptions.ServerException;
 import shared.interfaces.core.ICoreBill;
 import shared.interfaces.core.ICoreCharge;
 import shared.interfaces.core.ICoreProject;
+import shared.interfaces.core.ICoreProjectLiquidation;
 import shared.interfaces.core.ICoreUser;
 
 public class ServiceMobile extends ServiceBase{
@@ -32,11 +39,13 @@ public class ServiceMobile extends ServiceBase{
 	private ICoreProject iCoreProject = null;
 	private ICoreBill iCoreBill = null;
 	private ICoreCharge iCoreCharge = null;
+	private ICoreProjectLiquidation iCoreProjectLiquidation = null;
 	
 	private static UserBuilder userBuilser = new UserBuilder();
 	private static ProjectBuilder projectBuilder = new ProjectBuilder();
 	private static BillBuilder billBuilder = new BillBuilder();
 	private static ChargeBuilder chargeBuilder = new ChargeBuilder();
+	private static ProjectLiquidationBuilder projectLiquidationBuilder = new ProjectLiquidationBuilder();
 	
 	public ServiceMobile()
 	{
@@ -48,6 +57,7 @@ public class ServiceMobile extends ServiceBase{
 			iCoreProject = CoreProject.GetInstance();
 			iCoreBill = CoreBill.GetInstance();
 			iCoreCharge = CoreCharge.GetInstance();
+			iCoreProjectLiquidation = CoreProjectLiquidation.GetInstance();
 			
 		} catch (Exception e) {
 			ThrowGenericExceptionAndLogError(e);
@@ -166,6 +176,65 @@ public class ServiceMobile extends ServiceBase{
 		return false;
 	}
 	
+	public VOProjectLiquidation[] getProjectsLiquidationsWithMoreEarnings(Date dateFrom, Date dateTo, boolean isCurrencyDollar, int count){
+
+		VOProjectLiquidation[] voProjectsLiquidations= null;
+		try{
+			transactionLock.tryLock(Constants.DEFAULT_TRANSACTION_TIME, TimeUnit.SECONDS);			
+			
+			ArrayList<ProjectLiquidation> projectsLiquidations = iCoreProjectLiquidation.getProjectsWithMoreEarnings(dateFrom, dateTo, isCurrencyDollar, count);
+			voProjectsLiquidations = new VOProjectLiquidation[projectsLiquidations.size()];
+			
+			int i = 0;
+			for(ProjectLiquidation projectLiquidation : projectsLiquidations){
+				VOProjectLiquidation voProjectLiquidation = projectLiquidationBuilder.BuildVOObject(projectLiquidation);
+				voProjectLiquidation.setProject(projectBuilder.BuildVOObject(projectLiquidation.getProject()));
+				voProjectsLiquidations[i] = voProjectLiquidation;
+				i++;
+			}
+			return voProjectsLiquidations;
+			
+		} catch (ServerException e) {
+			ThrowServerExceptionAndLogError(e, "obtener liquidaciones de proyectos para mostrar");
+		} catch (InterruptedException e) {
+			throw new RuntimeException(Constants.TRANSACTION_ERROR);
+		} catch (Exception e) {
+			ThrowGenericExceptionAndLogError(e);
+		} finally {
+			transactionLock.unlock();
+		}
+		return null;
+	}
+	
+	public VOProjectLiquidation getProjectLiquidation(Date month, int projectId, int userContextId){
+		try{
+			transactionLock.tryLock(Constants.DEFAULT_TRANSACTION_TIME, TimeUnit.SECONDS);	
+			
+			User userContext = iCoreUser.getUser(userContextId);			
+			ProjectLiquidation liquidation =  iCoreProjectLiquidation.getProjectLiquidationByDate(month, projectId, userContext);
+			VOProjectLiquidation voLiquidation = null;
+			if(liquidation != null){
+				voLiquidation = projectLiquidationBuilder.BuildVOObject(liquidation);			
+				voLiquidation.setBills(billBuilder.BuildArrayVOObject(VOBill.class, liquidation.getBills()));
+				voLiquidation.setEmployees(projectBuilder.BuildVOEmployedProjects(liquidation.getEmployees()));
+				voLiquidation.setProject(projectBuilder.BuildVOObject(liquidation.getProject()));
+			}
+						
+			return voLiquidation;
+			
+		} catch (ServerException e) {
+			ThrowServerExceptionAndLogError(e, "obtener liquidaciones de proyectos para mostrar");
+		} catch (InterruptedException e) {
+			throw new RuntimeException(Constants.TRANSACTION_ERROR);
+		} catch (Exception e) {
+			ThrowGenericExceptionAndLogError(e);
+		} finally {
+			transactionLock.unlock();
+		}
+		return null;
+	}
+	
+
 	public String example(VOBill voBill)
 	{
 		return voBill.getId() + " - " + voBill.getCode() + " - " + voBill.getDescription();
